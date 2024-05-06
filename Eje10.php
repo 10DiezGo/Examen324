b3a90d1ec03925b1ce6503bcedaa259c4b859909
10DiezGo <diego.aliaga.candia@gmail.com>
2024-05-05 23:41:36 -0400

    Commit 3
    Ejercicio 10

---
+++ Eje10.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Hola Mundo</title>
+    </head>
+    <body>    
+        <?php
+            echo "¡Hola mundo!";
+        ?>
+    </body>    
+</html>
